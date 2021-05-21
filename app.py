0952f11518012faf24f5afbeba23b53b6b69c1e9
Neethu Mariya <nmariya@uwaterloo.ca>
2021-05-21 19:08:37 -0400

    updated

--- app.py
+++ app.py
@@ -6,20 +6,11 @@ from nltk.stem import PorterStemmer
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
-# from keras.models import Model, load_model, model_from_json
-# import tensorflow as tf
-# from keras.initializers import glorot_uniform
-# from keras import applications
-# from keras.applications.inception_resnet_v2 import InceptionResNetV2
-# from keras.layers import Input, Activation, Dropout, Flatten, Dense, BatchNormalization, Conv2D
-# from keras.optimizers import Adam
-# from keras import metrics
 import numpy as np
-# from PIL import Image
-# import cv2
 import matplotlib.pyplot as plt
 import warnings
 warnings.filterwarnings("ignore")
+
 # Text model - RNN with LSTM units
 class RNN_lstm(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
@@ -41,28 +32,6 @@ class RNN_lstm(nn.Module):
     def initHidden(self):
         return torch.zeros(1, self.hidden_size)
 
-# # Image model - CNN (InceptionResNetV2)
-# def create_model(input_shape, n_out):
-    
-#     pretrain_model = InceptionResNetV2(
-#         include_top=False, 
-#         weights='imagenet', 
-#         input_shape=input_shape)    
-    
-#     input_tensor = Input(shape=input_shape)
-#     bn = BatchNormalization()(input_tensor)
-#     x = pretrain_model(bn)
-#     x = Conv2D(128, kernel_size=(1,1), activation='relu')(x)
-#     x = Flatten()(x)
-#     x = Dropout(0.5)(x)
-#     x = Dense(512, activation='relu')(x)
-#     x = Dropout(0.5)(x)
-#     output = Dense(n_out, activation='sigmoid')(x)
-#     model = Model(input_tensor, output)
-    
-#     return model
-# i_model = create_model(input_shape=(299,299,3), n_out=27)
-
 # Dictionary
 with open('train_dictionary.pk', 'rb') as f:
     res2 = pickle.load(f)
@@ -75,9 +44,6 @@ n_hidden = 128
 # Load the saved model states
 model = RNN_lstm(n_words, n_hidden, n_categories)
 model.load_state_dict(torch.load('char_rnn_lstm_classification_model_state.pt'))
-
-# # Load model weights to 
-# i_model.load_weights("./ML_models/model.h5")
 
 app = Flask(__name__)
 
@@ -132,14 +98,6 @@ def evaluate_pickl(line_tensor, n_predictions=1):
             category_index_lstm = topi_lstm[0][i].item()
     return all_categories[category_index_lstm]
 
-# # Image preprocessing
-# def load_image(path, shape = (299,299,3)):
-#     '''This function converts the image to the Image-Net dataset standards'''
-#     image = np.array(Image.open(path+'.jpg'))
-#     image = cv2.resize(image, (shape[0], shape[1]))
-#     image = np.divide(image, 255)
-#     return image 
-
 @app.route('/predict', methods=['GET','POST'])
 def predict():
     # Text prediction
@@ -147,35 +105,7 @@ def predict():
     t = clean_doc(test_str)
     line_tensor = lineToTensor(t)
     out = evaluate_pickl(line_tensor)
-    return "Prediction from Text Classification model " + out
-
-# @app.route('/i-predict')
-# def i_predict():
-#     # image prediction
-#     image = load_image('images/999999')
-#     # imgplot = plt.imshow(image)
-#     # plt.show()
-#     score_predict = i_model.predict(image[np.newaxis])[0]
-#     label_predict = np.argmax(score_predict)
-#     return "Prediction from Image Classification model " + all_categories[label_predict]
-
-# @app.route('/predict')
-# def evaluate(line_tensor):
-#     hidden_lstm = rnn_lstm.initHidden()
-#     for i in range(line_tensor.size()[0]):
-#         output_lstm, hidden_lstm = rnn_lstm(line_tensor[i], hidden_lstm)
-#     return output_lstm
-# def predict(input_line, n_predictions=1):
-#     with torch.no_grad():
-#         output_lstm = evaluate(lineToTensor(input_line))
-#     return output_lstm
-# INPUT_SHAPE = (299,299,3)
-# def image_predict(path, INPUT_SHAPE):
-#     image = load_image(path, INPUT_SHAPE)
-#     score_predict_t = model.predict(image[np.newaxis])[0]
-#     label_predict = np.argmax(score_predict)
-#     predicted.append(label_predict)
-
+    return render_template('index.html',prediction_text="Prediction from Text Classification model ${}".format(out))
 
 # @app.route('/fruits')
 # def fruits():
