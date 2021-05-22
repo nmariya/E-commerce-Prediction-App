491a35c789ec8f8ecde8a761754792b375f1d005
Neethu Mariya <nmariya@uwaterloo.ca>
2021-05-21 20:34:02 -0400

    updated

--- app.py
+++ app.py
@@ -98,11 +98,14 @@ def evaluate_pickl(line_tensor, n_predictions=1):
             category_index_lstm = topi_lstm[0][i].item()
     return all_categories[category_index_lstm]
 
-@app.route('/predict', methods=['GET','POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
     # Text prediction
-    test_str = "Women	Brown	Summer	Casual	Senorita ColorBurst Brown Flats	"
-    t = clean_doc(test_str)
+    # test_str = "Women	Brown	Summer	Casual	Senorita ColorBurst Brown Flats	"
+    features=" "
+    for x in request.form.values():
+        features = features + " " + x
+    t = clean_doc(str(features))
     line_tensor = lineToTensor(t)
     out = evaluate_pickl(line_tensor)
     return render_template('index.html',prediction_text="Prediction from Text Classification model ${}".format(out))
